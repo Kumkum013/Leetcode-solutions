c8ba45c08d24644d98063870e1b62e48c1976460
Kumkum Kumari <kumkum2317@gmail.com>
2026-09-17 13:35:09 +0530

    Time: 0 ms (100%), Space: 48 MB (87.84%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseString(char[] s) {
+
+        char[] result = new char[s.length];
+
+        int j = 0;
+
+        for (int i = s.length - 1; i >= 0; i--) {
+
+            result[j] = s[i];
+            j++;
+        }
+
+        for (int i = 0; i < s.length; i++) {
+
+            s[i] = result[i];
+        }
+    }
+}
