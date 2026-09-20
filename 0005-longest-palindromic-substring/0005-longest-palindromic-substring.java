ef84f36135f86cf7a056d9eef5041d8a4e60fcee
Kumkum Kumari <kumkum2317@gmail.com>
2026-09-20 20:58:11 +0530

    Time: 1568 ms (9.85%), Space: 46.5 MB (39.29%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String longestPalindrome(String s) {
+
+        String answer = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            for (int j = i; j < s.length(); j++) {
+
+                if (isPalindrome(s, i, j)) {
+
+                    if (j - i + 1 > answer.length()) {
+                        answer = s.substring(i, j + 1);
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
