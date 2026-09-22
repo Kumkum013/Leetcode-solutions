9b857da282e0e77414d3763bde6b532dc7e11d95
Kumkum Kumari <kumkum2317@gmail.com>
2026-09-23 00:52:54 +0530

    Time: 13 ms (7.34%), Space: 42.6 MB (98.09%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,15 +1,48 @@
-class Solution {
-    public char findTheDifference(String s, String t) {
-        char answer = 0;
+// class Solution {
+//     public char findTheDifference(String s, String t) {
+//         char answer = 0;
 
-        for (char ch : s.toCharArray()) {
-            answer ^= ch;
-        }
+//         for (char ch : s.toCharArray()) {
+//             answer ^= ch;
+//         }
 
-        for (char ch : t.toCharArray()) {
-            answer ^= ch;
-        }
+//         for (char ch : t.toCharArray()) {
+//             answer ^= ch;
+//         }
 
-        return answer;
-    }
-}
+//         return answer;
+//     }
+// }
+
+
+// hashing
+import java.util.HashMap; 
+ 
+class Solution { 
+    public char findTheDifference(String s, String t) { 
+ 
+        HashMap<Character, Integer> map = new HashMap<>(); 
+ 
+        // Count characters in s 
+        for (int i = 0; i < s.length(); i++) { 
+ 
+            char ch = s.charAt(i); 
+ 
+            map.put(ch, map.getOrDefault(ch, 0) + 1); 
+        } 
+ 
+        // Compare characters in t 
+        for (int i = 0; i < t.length(); i++) { 
+ 
+            char ch = t.charAt(i); 
+ 
+            if (!map.containsKey(ch) || map.get(ch) == 0) { 
+                return ch; 
+            } 
+ 
+            map.put(ch, map.get(ch) - 1); 
+        } 
+ 
+        return ' '; 
+    } 
+} 
